4bfb3bfc0a949854c31d392bcfc0dc2c3a24b8ec
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2021-12-11 15:34:38 +0700

    new branch

--- smoke/data/__init__.py
+++
@@ -1,2 +0,0 @@
-from .build import make_data_loader
-from .build import build_test_loader
--- smoke/data/datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .kitti import KITTIDataset
-from .concat_dataset import ConcatDataset
-
-__all__ = ["KITTIDataset"]
